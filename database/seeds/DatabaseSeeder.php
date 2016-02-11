66eb66f723ac4884a6a6f19179aab7a1c8541e71
Nick Dunn <nickdunn2@gmail.com>
2016-02-11 10:48:35 -0600

    fix post_user seeding issue, but still not random

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Seeder;
 use App\User;
 use App\Post;
 use Illuminate\Support\Facades\DB;
-use Faker\Generator;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,14 +17,13 @@ class DatabaseSeeder extends Seeder
         // $this->call(UserTableSeeder::class);
         User::truncate();
         Post::truncate();
-        // DB::table('post_user')->truncate();
+        DB::table('post_user')->truncate();
 
         factory(User::class, 50)
             ->create()
             ->each(function($user) {
-                $postIds = factory(App\Post::class, 50)->create()->pluck('id')->toArray();
+                $postIds = factory(App\Post::class, 2)->create()->pluck('id')->toArray();
                 $user->likes()->sync($postIds);
             });
-        factory(Post::class, 50)->create();
     }
 }
