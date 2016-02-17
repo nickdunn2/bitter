41c4eb3c7b5245ec8d1a89fc3b8883c9498a3704
Nick Dunn <nickdunn2@gmail.com>
2016-02-17 11:25:38 -0600

    add controllers with basic index and show methods

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('user', 'UserController');
+Route::resource('post', 'PostController');
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
